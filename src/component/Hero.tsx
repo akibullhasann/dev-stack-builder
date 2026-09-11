45a871c23a349c06f3ad384a2e08d9565b7563a7
akibullhasann <akibullhasann@gmail.com>
2026-09-12 00:48:10 +0100

    try to fix some problem with nav and hero  Add technology data in json

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -3,10 +3,10 @@ import BannerImg from "../assets/banner-stack.png"
 const Hero = () => {
     return (
        <div className="mt-20">
-           <div className="grid grid-cols-1 px-2.5 md:px-0 md:grid-cols-3 justify-items-center items-center gap-4 container mx-auto ">
+           <div className="grid grid-cols-1 px-2.5  md:grid-cols-3 justify-items-center items-center gap-4 container mx-auto ">
             <div className="flex flex-col justify-between gap-5 md:col-span-2">
-                <h2 className="text-center md:text-left text-5xl md:text-7xl font-bold">Build Your Perfect <span className="block gradient-text text-5xl md:text-7xl">Development Stack</span></h2>
-                <p className="font-serif">Discover, explore, and choose the best technologies for your projects. Build your perfect tech stack, learn new tools, and take your development journey to the next level.
+                <h2 className="text-center md:text-left text-4xl md:text-7xl font-bold">Build Your Perfect <span className="block gradient-text text-4xl md:text-7xl">Development Stack</span></h2>
+                <p className="font-serif text-center md:text-left">Discover, explore, and choose the best technologies for your projects. Build your perfect tech stack, learn new tools, and take your development journey to the next level.
                 </p>
                 <div className="flex gap-6 justify-center md:justify-start">
                     <button className="btn-gradient ">Explore Tecnologies</button>
--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -4,9 +4,9 @@ import HamIcon from "../assets/hamburger.png"
 const NavBar = () => {
     return (
         <nav className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-50">
-            <div className="flex justify-between py-3 container mx-auto items-center px-5 md:px-0">
+            <div className="flex justify-between py-3 container mx-auto items-center px-5">
                 <img src={HamIcon} className="w-6.5 h-4 md:hidden"></img>
-                <img src={Logo}></img>
+                <img src={Logo} className="w-22 md:w-auto"></img>
                 <ul className="md:flex justify-between gap-6 items-center hidden">
                     <li><a href="">Home</a></li>
                     <li><a href="">Technologies</a></li>
@@ -14,9 +14,9 @@ const NavBar = () => {
                     <li><a href="">About</a></li>
                     <li><a href="">Contact</a></li>
                 </ul>
-                <div className="flex gap-3">
-                    <button className="text-sm md:text-base">Sign In</button>
-                    <button className="text-sm md:text-base
+                <div className="flex gap-3 shrink text-center">
+                    <button className="text-xs md:text-base">Sign In</button>
+                    <button className="text-xs md:text-base
                     
                     btn-gradient btn-gradient:hover cursor-pointer">Sign Up</button>
                 </div>
