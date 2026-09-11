25547d53f72b16a545a7b7d155af0b649fd4a5dd
akibullhasann <akibullhasann@gmail.com>
2026-09-11 15:33:13 +0100

    NavBar componet has been created

---
+++ src/component/NavBar.tsx
@@ -0,0 +1,28 @@
+import Logo from "../assets/logo-text.png"
+import HamIcon from "../assets/hamburger.png"
+
+const NavBar = () => {
+    return (
+        <nav className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-50">
+            <div className="flex justify-between py-3 container mx-auto items-center px-5 md:px-0">
+                <img src={HamIcon} className="w-6.5 h-4 md:hidden"></img>
+                <img src={Logo}></img>
+                <ul className="md:flex justify-between gap-6 items-center hidden">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Technologies</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <div className="flex gap-3">
+                    <button className="text-sm md:text-base">Sign In</button>
+                    <button className="text-sm md:text-base
+                    
+                    btn-gradient btn-gradient:hover cursor-pointer">Sign Up</button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
