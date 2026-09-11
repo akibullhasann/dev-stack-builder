f2e5726edcb86b8c105ef96bee5ab4b8c7a4dd04
akibullhasann <akibullhasann@gmail.com>
2026-09-11 23:57:19 +0100

    worked on Hero banner section & tried to complete it

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import Hero from './component/Hero'
 import NavBar from './component/NavBar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Hero></Hero>
     </>
   )
 }
---
+++ src/component/Hero.tsx
@@ -0,0 +1,24 @@
+import BannerImg from "../assets/banner-stack.png"
+
+const Hero = () => {
+    return (
+       <div className="mt-20">
+           <div className="grid grid-cols-1 px-2.5 md:px-0 md:grid-cols-3 justify-items-center items-center gap-4 container mx-auto ">
+            <div className="flex flex-col justify-between gap-5 md:col-span-2">
+                <h2 className="text-center md:text-left text-5xl md:text-7xl font-bold">Build Your Perfect <span className="block gradient-text text-5xl md:text-7xl">Development Stack</span></h2>
+                <p className="font-serif">Discover, explore, and choose the best technologies for your projects. Build your perfect tech stack, learn new tools, and take your development journey to the next level.
+                </p>
+                <div className="flex gap-6 justify-center md:justify-start">
+                    <button className="btn-gradient ">Explore Tecnologies</button>
+                    <button className="border px-5 rounded-2xl font-semibold border-gray-300">Learn More</button>
+                </div>
+            </div>
+            <div>
+                <img src={BannerImg} alt="" />
+            </div>
+        </div>
+       </div>
+    );
+};
+
+export default Hero;
